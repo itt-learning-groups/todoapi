29ab16b56d7b4bd70d19cca713122ceb83c415af
B Randy King <brk@kount.com>
2021-08-16 02:19:59 -0600

    Aug 13: Deploy to dev/qa/prod via k8s namespaces

--- build/k8s/deploy.sh
+++ build/k8s/deploy.sh
@@ -3,21 +3,24 @@
 # Get the deployment environment ("dev", "qa", or "prod") from a command-line argument
 ENV=$1
 
-# Deploy the `secret` that will hold the DB username/password
-kubectl create secret generic todoapi-configs-"${ENV}" --from-file="${ENV}"/secrets -n default
-kubectl label secret todoapi-configs-"${ENV}" app=todoapi env=dev
+# Deploy the environment `namespace`
+kubectl apply -f ./"${ENV}"/namespace.yaml
 
-# Deploy the `config-map` that will hold the other DB config values
-kubectl apply -f ./"${ENV}"/config.yaml -n default
+# Deploy the `secret` that will hold the DB username/password in this namespace
+kubectl create secret generic todoapi-configs --from-file="${ENV}"/secrets -n "${ENV}"
+kubectl label secret todoapi-configs app=todoapi env=dev -n "${ENV}"
 
-# Deploy the `deployment` and `service`
-kubectl apply -f ./"${ENV}"/service.yaml -n default
-kubectl apply -f ./"${ENV}"/deployment.yaml -n default
+# Deploy the `config-map` that will hold the other DB config values in this namespace
+kubectl apply -f ./"${ENV}"/config.yaml -n "${ENV}"
+
+# Deploy the `deployment` and `service` in this namespace
+kubectl apply -f ./"${ENV}"/service.yaml -n "${ENV}"
+kubectl apply -f ./"${ENV}"/deployment.yaml -n "${ENV}"
 
 # Wait a few seconds for the service loadbalancer hostname to be available
 sleep 5
 
-# Print out the service hostname so we can test it (browser, curl, Postman)
+# Print out the service hostname so we can test it (browser, curl, Postman) for this namespace
 printf "\nloadbalancer hostname: "
-kubectl get service todoapi-"${ENV}" -n default -o jsonpath="{.status.loadBalancer.ingress[*].hostname}"
+kubectl get service todoapi -n "${ENV}" -o jsonpath="{.status.loadBalancer.ingress[*].hostname}"
 echo ""
