f4fd1362fa7364520e05dc7f2f1b48cb1fddf35b
B Randy King <brk@kount.com>
2021-08-19 20:05:52 -0600

    Sept 1: Add volume-mount for DB secrets

--- cmd/todo_api_server/main.go
+++ cmd/todo_api_server/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/us-learn-and-devops/todoapi/cmd/todo_api_server/handlers"
 	"github.com/us-learn-and-devops/todoapi/configs"
 	envcfg "github.com/us-learn-and-devops/todoapi/pkg/envconfig"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -13,12 +14,26 @@ func main() {
 	cfgs := &configs.Settings{}
 	err := envcfg.Unmarshal(cfgs)
 	if err != nil {
-		log.Fatalf("Failed to get configs: %s", err)
+		log.Fatalf("failed to get configs: %s", err)
 	}
 
-	tl, err:= handlers.NewTodoListHandler(cfgs)
+	var dbCreds handlers.DBCredentials
+
+	dbUsername, err := ioutil.ReadFile(cfgs.DatabaseUserNameFilePath)
 	if err != nil {
-		log.Fatalf("main.main failed to create TodoListHandler: %v", err)
+		log.Fatalf("failed to get DB username: %v", err)
+	}
+	dbCreds.Username = string(dbUsername)
+
+	dbPswd, err := ioutil.ReadFile(cfgs.DatabasePswdFilePath)
+	if err != nil {
+		log.Fatalf("failed to get DB password: %v", err)
+	}
+	dbCreds.Password = string(dbPswd)
+
+	tl, err:= handlers.NewTodoListHandler(cfgs, dbCreds)
+	if err != nil {
+		log.Fatalf("failed to create TodoListHandler: %v", err)
 	}
 
 	r := handlers.NewRouter(tl)
